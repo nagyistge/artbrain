f5f6839ec235485118a45af9ed0ff4ab9697ac21
vsoch <vsochat@stanford.edu>
2015-12-21 09:29:08 -0800

    updating script to only run if nifti is generated

--- artbrain/art.py
+++ artbrain/art.py
@@ -24,6 +24,7 @@ def generate(template,output_folder=None,orthoview="axial",view=True,port=None,r
 
     :param template: the template image to generate brainart for! Only png has been tested. This image will be placed on all slices.
     :param output_folder: output folder for the final file, including a nifti image and web view. If not specified, will use TMP. To disable the web page, set view=False
+    :param view: show in browser. Default is True.
     :param orthoview: one of sagittal, coronal, or axial. Currently only supported is axial.
     '''
     if output_folder == None:
@@ -39,31 +40,32 @@ def generate(template,output_folder=None,orthoview="axial",view=True,port=None,r
     nii = map_to_brain(image_3d,brain_mask,axis=orthoview,rotations=rotations)
 
     # Save output to file
-    new_image_name = os.path.splitext(os.path.split(template)[-1])[0]
-    new_image = "%s/%s.nii.gz" %(output_folder,new_image_name)
-    nibabel.save(nii,new_image)
+    if isinstance(nii,nibabel.Nifti1Image):
+        new_image_name = os.path.splitext(os.path.split(template)[-1])[0]
+        new_image = "%s/%s.nii.gz" %(output_folder,new_image_name)
+        nibabel.save(nii,new_image)
 
-    print "Nifti image saved to %s" %(new_image)
+        print "Nifti image saved to %s" %(new_image)
 
-    # Open to show in browser
-    if view==True:
-        html_template = get_template("index")
-        html_template = sub_template(html_template,"DATA","%s.nii.gz" %new_image_name)
-        output_html = "%s/index.html" %output_folder
-        save_template(output_html,html_template)
+        # Open to show in browser
+        if view==True:
+            html_template = get_template("index")
+            html_template = sub_template(html_template,"DATA","%s.nii.gz" %new_image_name)
+            output_html = "%s/index.html" %output_folder
+            save_template(output_html,html_template)
 
-        # Copy brain image template
-        shutil.copyfile(os.path.abspath(os.path.join("%s" %base,"data/MNI152_T1_2mm_brain.nii.gz")),"%s/MNI152_T1_2mm_brain.nii.gz" %output_folder)
+            # Copy brain image template
+            shutil.copyfile(os.path.abspath(os.path.join("%s" %base,"data/MNI152_T1_2mm_brain.nii.gz")),"%s/MNI152_T1_2mm_brain.nii.gz" %output_folder)
         
-        try:
-            os.chdir(output_folder)
-            if port == None:
-                port = choice(range(8000,9999),1)[0]
-            Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-            httpd = SocketServer.TCPServer(("", port), Handler)
-            print "Art brain viewer at localhost:%s" %port
-            webbrowser.open("http://localhost:%s" %(port))
-            httpd.serve_forever()
-        except:
-            print "Stopping web server..."
-            httpd.server_close()
+            try:
+                os.chdir(output_folder)
+                if port == None:
+                    port = choice(range(8000,9999),1)[0]
+                Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+                httpd = SocketServer.TCPServer(("", port), Handler)
+                print "Art brain viewer at localhost:%s" %port
+                webbrowser.open("http://localhost:%s" %(port))
+                httpd.serve_forever()
+            except:
+                print "Stopping web server..."
+                httpd.server_close()
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="artbrain",
 
     # Version number (initial):
-    version="1.0.0",
+    version="1.0.1",
 
     # Application author details:
     author="vsoch",
